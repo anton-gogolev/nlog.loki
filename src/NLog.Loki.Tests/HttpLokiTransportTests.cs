b47b5432eabe4489684f30c8066041ca10825fc3
Corentin Altepe <corentin.altepe@gmail.com>
2022-03-12 12:07:02 +0100

    Implemented test on the CompressedContent for all 4 levels.

--- src/NLog.Loki.Tests/HttpLokiTransportTests.cs
+++ src/NLog.Loki.Tests/HttpLokiTransportTests.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO.Compression;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using NLog.Loki.Model;
@@ -13,17 +15,17 @@ namespace NLog.Loki.Tests;
 
 public class HttpLokiTransportTests
 {
-    private static List<LokiEvent> CreateLokiEvents()
+    private static IEnumerable<LokiEvent> CreateLokiEvents(int numberEvents = 3)
     {
         var date = new DateTime(2021, 12, 27, 9, 48, 26, DateTimeKind.Utc);
-        return new List<LokiEvent> {
-            new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")),
-                date, "Info|Receive message from A with destination B."),
-            new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")),
-                date + TimeSpan.FromSeconds(2.2), "Info|Another event has occured here."),
-            new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")),
-                date - TimeSpan.FromSeconds(0.9), "Info|Event from another stream."),
-        };
+        for(var i = 0; i < numberEvents; i++)
+        {
+            yield return new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")), date, "Info|Receive message from A with destination B.");
+            i++;
+            yield return new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")), date + TimeSpan.FromSeconds(2.2), "Info|Another event has occured here.");
+            i++;
+            yield return new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")), date - TimeSpan.FromSeconds(0.9), "Info|Event from another stream.");
+        }
     }
 
     [Test]
@@ -84,7 +86,7 @@ public class HttpLokiTransportTests
     public async Task SerializeMessageToHttpLokiSingleEvent()
     {
         // Prepare the event to be sent to loki
-        var lokiEvent = CreateLokiEvents()[2];
+        var lokiEvent = CreateLokiEvents().ToList()[2];
 
         // Configure the ILokiHttpClient such that we intercept the JSON content and simulate an OK response from Loki.
         string serializedJsonMessage = null;
@@ -141,5 +143,43 @@ public class HttpLokiTransportTests
 #if NET6_0_OR_GREATER
         Assert.AreEqual(HttpStatusCode.Conflict, exception.StatusCode);
 #endif
+    }
+
+    [Test]
+    [TestCase(CompressionLevel.NoCompression)]
+    [TestCase(CompressionLevel.Fastest)]
+    [TestCase(CompressionLevel.Optimal)]
+#if NET6_0_OR_GREATER
+    [TestCase(CompressionLevel.SmallestSize)]
+#endif
+    public async Task CompressMessage(CompressionLevel level)
+    {
+        // Prepare the events to be sent to loki
+        var events = CreateLokiEvents(3);
+
+        // Configure the ILokiHttpClient such that we intercept the JSON content and simulate an OK response from Loki.
+        string serializedJsonMessage = null;
+        var httpClient = new Mock<ILokiHttpClient>();
+        _ = httpClient.Setup(c => c.PostAsync("loki/api/v1/push", It.IsAny<HttpContent>()))
+            .Returns<string, HttpContent>(async (s, content) =>
+            {
+                // Intercept the gzipped json content so that we can verify it.
+                var stream = await content.ReadAsStreamAsync();
+                if(level != CompressionLevel.NoCompression)
+                    stream = new GZipStream(stream, CompressionMode.Decompress);
+                var buffer = new byte[128000];
+                var length = stream.Read(buffer, 0, buffer.Length);
+                serializedJsonMessage = Encoding.UTF8.GetString(buffer, 0, length);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            });
+
+        // Send the logging request
+        var transport = new HttpLokiTransport(httpClient.Object, orderWrites: false, level);
+        await transport.WriteLogEventsAsync(events).ConfigureAwait(false);
+
+        // Verify the json message format
+        Assert.AreEqual(
+            "{\"streams\":[{\"stream\":{\"env\":\"unittest\",\"job\":\"Job1\"},\"values\":[[\"1640598506000000000\",\"Info|Receive message from A with destination B.\"],[\"1640598508200000000\",\"Info|Another event has occured here.\"],[\"1640598505100000000\",\"Info|Event from another stream.\"]]}]}",
+            serializedJsonMessage);
     }
 }
