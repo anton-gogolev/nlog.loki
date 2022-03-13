17101a2f579434d0e9c0e7442308286c197dc452
Corentin Altepe <corentin.altepe@gmail.com>
2022-03-13 09:30:24 +0100

    Added CompressionLevel as a variable to benchmarks.

--- src/Benchmark/Transport.cs
+++ src/Benchmark/Transport.cs
@@ -12,13 +12,20 @@ namespace Benchmark;
 [MemoryDiagnoser]
 public class Transport
 {
+    [Params(CompressionLevel.NoCompression, CompressionLevel.Fastest, CompressionLevel.Optimal,
+#if NET6_0_OR_GREATER
+                CompressionLevel.SmallestSize
+#endif
+        )]
+    public CompressionLevel CompressionLevel { get; set; }
+
     private readonly IList<LokiEvent> _manyLokiEvents;
     private readonly IList<LokiEvent> _lokiEvents = new List<LokiEvent> {
         new(
             new LokiLabels(new LokiLabel("env", "benchmark"), new LokiLabel("job", "WriteLogEventsAsync")),
             DateTime.Now,
             "Info|Receive message from \"A\" with destination \"B\".")};
-    private readonly HttpLokiTransport _transport = new(new LokiHttpClient(
+    private HttpLokiTransport _transport = new(new LokiHttpClient(
         new HttpClient { BaseAddress = new Uri("http://localhost:3100") }), false, CompressionLevel.NoCompression);
 
     public Transport()
@@ -26,6 +33,12 @@ public class Transport
         _manyLokiEvents = new List<LokiEvent>(100);
         for(var i = 0; i < 100; i++)
             _manyLokiEvents.Add(new LokiEvent(_lokiEvents[0].Labels, DateTime.Now, _lokiEvents[0].Line));
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _transport = new(new LokiHttpClient(new HttpClient { BaseAddress = new Uri("http://localhost:3100") }), false, CompressionLevel);
     }
 
     [Benchmark]
