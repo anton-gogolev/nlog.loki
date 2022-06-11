230df916770b64efe84fc9c5ab210fecf1d78a9c
Corentin Altepe <corentin.altepe@gmail.com>
2022-06-11 13:03:19 +0200

    Added parameter on serializer benchmarks.

--- src/Benchmark/LokiEventsSerializer.cs
+++ src/Benchmark/LokiEventsSerializer.cs
@@ -15,7 +15,7 @@ public class LokiEventsSerializer
     private readonly JsonSerializerOptions _serializerOptionsWithOrdering = new();
 
     private readonly LokiEvent _event;
-    private readonly IEnumerable<LokiEvent> _manyLokiEvents;
+    private IEnumerable<LokiEvent> _manyLokiEvents;
 
     public LokiEventsSerializer()
     {
@@ -23,10 +23,7 @@ public class LokiEventsSerializer
             new LokiLabels(new HashSet<LokiLabel> { new LokiLabel("env", "benchmark"), new LokiLabel("job", "WriteLogEventsAsync") }),
             DateTime.Now,
             "Info|Receive message from \"A\" with destination \"B\".");
-        var events = new List<LokiEvent>(100);
-        for(var i = 0; i < 100; i++)
-            events.Add(new LokiEvent(_event.Labels, DateTime.Now, _event.Line));
-        _manyLokiEvents = events;
+
 
         _serializerOptionsWithoutOrdering = new JsonSerializerOptions();
         _serializerOptionsWithoutOrdering.Converters.Add(new NLog.Loki.LokiEventsSerializer(orderWrites: false));
@@ -37,6 +34,18 @@ public class LokiEventsSerializer
         _serializerOptionsWithOrdering.Converters.Add(new NLog.Loki.LokiEventSerializer());
     }
 
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var events = new List<LokiEvent>(N);
+        for(var i = 0; i < N; i++)
+            events.Add(new LokiEvent(_event.Labels, DateTime.Now, _event.Line));
+        _manyLokiEvents = events;
+    }
+
+    [Params(/*1, 10,*/ 100, 1000)]
+    public int N { get; set; }
+
     [Benchmark]
     public void SerializeManyEventsWithoutOrdering()
     {
@@ -44,17 +53,17 @@ public class LokiEventsSerializer
         JsonSerializer.Serialize(_stream, _manyLokiEvents, _serializerOptionsWithoutOrdering);
     }
 
-    [Benchmark]
-    public void SerializeManyEventsWithOrdering()
-    {
-        _stream.Position = 0;
-        JsonSerializer.Serialize(_stream, _manyLokiEvents, _serializerOptionsWithOrdering);
-    }
+    //[Benchmark]
+    //public void SerializeManyEventsWithOrdering()
+    //{
+    //    _stream.Position = 0;
+    //    JsonSerializer.Serialize(_stream, _manyLokiEvents, _serializerOptionsWithOrdering);
+    //}
 
-    [Benchmark]
-    public void SerializeSingleEvent()
-    {
-        _stream.Position = 0;
-        JsonSerializer.Serialize(_stream, _event, _serializerOptionsWithoutOrdering);
-    }
+    //[Benchmark]
+    //public void SerializeSingleEvent()
+    //{
+    //    _stream.Position = 0;
+    //    JsonSerializer.Serialize(_stream, _event, _serializerOptionsWithoutOrdering);
+    //}
 }
